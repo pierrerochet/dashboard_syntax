8029f957f2fc5b7efe2102763df8bb4ccacc5c1c
Alican Yüksel <alicanyuksel@outlook.com>
2019-11-15 00:06:00 +0100

    first commit -alican

---
+++ database.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 10:10:55 2019
+
+@author: rochet
+"""
+import numpy as np
+import base64
+
+def load(file):
+    _ , content_string = file.split(',')
+    decoded = base64.b64decode(content_string)
+    return decoded.decode("UTF8")
+
+def create(content):
+    database = []
+    save = {"form":[], "lemma":[], "pos":[], "dep":[], "size":0}
+    input_file = content.split("\n")
+    for l in input_file:
+        if l !="":
+            if "# sent_id" in l: save['sent_id'] = l[12:]
+            elif "# text" in l: save['text'] = l[9:]
+            else:
+                elements = l.split('\t')
+                save['form'].append(elements[1])
+                save['lemma'].append(elements[2])
+                save['pos'].append(elements[3])
+                save['dep'].append(elements[7])
+                save['size'] += 1
+        else:
+            database.append(save)
+            save = {"form":[], "lemma":[], "pos":[], "dep":[], "size":0}
+    return database
+
+
+def stats(database):
+    pos_stats = {}
+    dep_stats = {}
+    total_sentence = 0
+    seq_num_token = []
+    seq_token = []
+    for seq in database:
+        total_sentence += 1
+        seq_num_token.append(seq["size"])
+        seq_token += seq["form"]
+        for pos in seq["pos"]:
+            pos_stats[pos] = pos_stats.get(pos, 0) + 1
+        for dep in seq["dep"]:
+            dep_stats[dep] = dep_stats.get(dep, 0) + 1
+    diversity_token = len(set(seq_token))
+    total_token = sum(seq_num_token)
+    mean_token = np.mean(np.array(seq_num_token))
+    std_token = np.std(np.array(seq_num_token))
+    std_token = "{:.2f}".format(std_token)
+    g_stats = {"total":total_token,"diversity":diversity_token,"mean":mean_token,"std":std_token, "sentence":total_sentence }
+
+    return g_stats, pos_stats, dep_stats
